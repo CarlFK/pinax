9e7025eb84cc684575178f9233f965532e504f2f
Carl Karsten <carl@gw42.(none)>
2009-11-12 01:34:43 -0600

    made it work for me, kept it generic

--- pinax/projects/social_project/apps/friends_app/forms.py
+++ pinax/projects/social_project/apps/friends_app/forms.py
@@ -14,3 +14,56 @@ class ImportVCardForm(forms.Form):
         imported, total = import_vcards(self.cleaned_data["vcard_file"].content, user)
         return imported, total
         
+from friends.forms import InviteFriendForm as friends_InviteFriendForm
+from friends.forms import JoinRequestForm as friends_JoinRequestForm
+
+class InviteFriendForm(friends_JoinRequestForm,friends_InviteFriendForm):
+    """
+    user enters email addres.
+    if it belongs to an exising user, 
+        the friends_app InviteFriendForm code is used to send a frend invite
+    else  
+        the friends_app JoinRequestForm code is used to email an join invite
+    """
+
+    def clean_to_user(self):
+        """
+        override the "must be valid user" rule.
+        if it isn't, an email will be sent to invite them 
+        and the friend link will be setup if they join.
+        """
+        return self.cleaned_data["to_user"]
+
+    def clean_email(self):
+        """
+        override this too.
+        if it is existing user, then it will send a friend request.
+        """
+        return self.cleaned_data["email"]
+
+# careful messing with .clean
+# current code works around a bug:
+# http://code.pinaxproject.com/tasks/task/522/
+
+    def clean(self):
+        users=User.objects.filter(email=self.cleaned_data["email"])
+        if users:
+            self.cleaned_data["to_user"]=users[0].username
+            # existing user, check for friend link
+            return friends_InviteFriendForm.clean(self)
+        else:
+            # new user, check for pending invite
+            return friends_JoinRequestForm.clean(self)
+
+
+    def save(self,user):
+        if self.cleaned_data["to_user"]:
+            # existing user, send friend request
+            return friends_InviteFriendForm.save(self)
+        else:
+            # no user, send invite
+            return friends_JoinRequestForm.save(self,user)
+
+
+            
+
--- pinax/projects/social_project/apps/friends_app/views.py
+++ pinax/projects/social_project/apps/friends_app/views.py
@@ -7,14 +7,23 @@ from django.http import HttpResponseRedirect
 from django.utils.translation import ugettext_lazy as _
 
 from friends.models import *
-from friends.forms import JoinRequestForm
+from friends.forms import InviteFriendForm as friends_InviteFriendForm
+from friends.forms import JoinRequestForm as friends_JoinRequestForm
 from friends_app.forms import ImportVCardForm
-from account.forms import SignupForm
+
+from friends_app.forms import InviteFriendForm
+from accounts import models, forms
+
 from friends.importer import import_yahoo, import_google
 
 # @@@ if made more generic these could be moved to django-friends proper
 
 def friends(request, template_name="friends_app/invitations.html"):
+    """
+    Show lists of friend requests sent/received and invites sent/status.
+    'send invite' link
+    """
+    friend_form=InviteFriendForm()
     if request.method == "POST":
         invitation_id = request.POST.get("invitation", None)
         if request.POST["action"] == "accept":
@@ -33,19 +42,36 @@ def friends(request, template_name="friends_app/invitations.html"):
                     request.user.message_set.create(message=_("Declined friendship request from %(from_user)s") % {'from_user': invitation.from_user})
             except FriendshipInvitation.DoesNotExist:
                 pass
-    
+        elif request.POST["action"] == "friend":
+            friend_form=InviteFriendForm(request.user,request.POST)
+            if friend_form.is_valid():
+                friend_form.save(request.user)
+            else:
+                pass
+                # print friend_form.errors
+
     invites_received = request.user.invitations_to.invitations().order_by("-sent")
     invites_sent = request.user.invitations_from.invitations().order_by("-sent")
     joins_sent = request.user.join_from.all().order_by("-sent")
     
+    contacts=Contact.objects.filter(user=request.user)
+
+    friends = Friendship.objects.friends_for_user(request.user)
+
     return render_to_response(template_name, {
         "invites_received": invites_received,
+        'friend_form':friend_form,
         "invites_sent": invites_sent,
         "joins_sent": joins_sent,
+        "contacts": contacts,
+        "friends": friends,
     }, context_instance=RequestContext(request))
 friends = login_required(friends)
 
-def invite(request, form_class=JoinRequestForm, **kwargs):
+def invite(request, form_class=friends_JoinRequestForm, **kwargs):
+    """
+    send 'join this site' message
+    """
     template_name = kwargs.get("template_name", "friends_app/invite.html")
     if request.is_ajax():
         template_name = kwargs.get(
@@ -64,20 +90,36 @@ def invite(request, form_class=JoinRequestForm, **kwargs):
         }, context_instance=RequestContext(request))
 invite = login_required(invite)
 
-def accept_join(request, confirmation_key, form_class=SignupForm,
-        template_name="account/signup.html"):
+def accept_join(request, confirmation_key, 
+        form_class=forms.UserRegistrationForm,
+        template_name="registration/registration_form.html"):
+    """
+    'join this site' link comes here.
+    """
+
+    # template_name="friends_app/signup.html"
     join_invitation = get_object_or_404(JoinInvitation, confirmation_key = confirmation_key.lower())
     if request.user.is_authenticated():
-        return render_to_response("account/signup.html", {
+        return render_to_response("registration/registration_form.html", {
         }, context_instance=RequestContext(request))
     else:
-        form = form_class(initial={"email": join_invitation.contact.email, "confirmation_key": join_invitation.confirmation_key })
+        initial={
+            "email1": join_invitation.contact.email, 
+            "email2": join_invitation.contact.email, }
+        if confirmation_key:
+# the key will be in reqest.POST if the form errors and they resubmit.
+            initial["confirmation_key"] = confirmation_key
+        form = form_class(request,initial=initial)
         return render_to_response(template_name, {
             "form": form,
         }, context_instance=RequestContext(request))
 
 def contacts(request, form_class=ImportVCardForm,
         template_name="friends_app/contacts.html"):
+    """
+    get contact lists from yahoo, google
+    send invites
+    """
     if request.method == "POST":
         if request.POST["action"] == "upload_vcard":
             import_vcard_form = form_class(request.POST, request.FILES)
@@ -132,4 +174,6 @@ def friends_objects(request, template_name, friends_objects_function, extra_cont
         dictionary[name] = func(request)
     
     return render_to_response(template_name, dictionary, context_instance=RequestContext(request))
+
 friends_objects = login_required(friends_objects)
+
